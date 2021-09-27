9a1613d0898871968b3f1094504157c5057daf28
Nguyễn Thị Thúy <thuynguyen03091999@gmail.com>
2021-09-27 13:12:53 +0700

    Add hero tag for image

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -23,9 +23,12 @@ class ProductDetailScreen extends StatelessWidget {
           Container(
             height: 300,
             width: double.infinity,
-            child: Image.network(
-              loadProduct.imageUrl,
-              fit: BoxFit.cover,
+            child: Hero(
+              tag: productId,
+              child: Image.network(
+                loadProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
           SizedBox(
